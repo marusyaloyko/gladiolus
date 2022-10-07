0c7e0809e52117f7ccfd69fc0320d0c9df3ff124
marusyaloyko <115215237+marusyaloyko@users.noreply.github.com>
2022-10-07 11:54:50 +0300

    Delete TestAuthor.java

--- TestAuthor.java
+++
@@ -1,8 +0,0 @@
-public class TestAuthor {
-    public static void main(String[] args) {
-        Author m;
-        m=new Author("Мария","kikilia77ichbin8@gmail.ru",'f');
-        System.out.println(m.getEmail());
-        System.out.println(m.toString());
-    }
-}
